9759b6599001a5767ea55de7bf3f8dcb83c542a5
Milan Tyagi <milantyagi2004@gmail.com>
2026-03-05 12:00:04 +0530

    fixes  in booking reposity

--- src/main/java/com/chat/bot/repository/BookingRepository.java
+++ src/main/java/com/chat/bot/repository/BookingRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,String> {
@@ -13,4 +14,8 @@ public interface BookingRepository extends JpaRepository<Booking,String> {
             LocalDate date,
             String time
     );
+
+    List<Booking> findByAppointmentDate(LocalDate date);
+
+    List<Booking> findByLocation(String location);
 }
